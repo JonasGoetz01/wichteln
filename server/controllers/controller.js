57176fbd48dc536337a52970ad7e170bc18456bf
JonasGoetz01 <65551807+JonasGoetz01@users.noreply.github.com>
2021-12-05 15:06:07 +0100

    recreate host

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 // Connection Pool
 let connection = mysql.createConnection({
-  host: "136.243.152.18",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
