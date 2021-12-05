052c2247cc73565a643a03a051837b067897f8e0
Jonas Götz <jonas.goetz01@web.de>
2021-12-05 13:40:05 +0100

    update paths

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,11 +7,11 @@ router.get('/', controller.home);
 router.get('/wichteln/', controller.makeAssignments);
 router.get('/wichteln/usermanagement/', controller.viewuser);
 router.post('/wichteln/usermanagement/', controller.finduser);
-router.get('/usermanagement/adduser', controller.formuser);
-router.post('/usermanagement/adduser', controller.createuser);
-router.get('/usermanagement/edituser/:id', controller.edituser);
-router.post('/usermanagement/edituser/:id', controller.updateuser);
-router.get('/usermanagement/viewuser/:id', controller.viewalluser);
-router.get('/usermanagement/:id',controller.deleteuser);
+router.get('/wichteln/usermanagement/adduser', controller.formuser);
+router.post('/wichteln/usermanagement/adduser', controller.createuser);
+router.get('/wichteln/usermanagement/edituser/:id', controller.edituser);
+router.post('/wichteln/usermanagement/edituser/:id', controller.updateuser);
+router.get('/wichteln/usermanagement/viewuser/:id', controller.viewalluser);
+router.get('/wichteln/usermanagement/:id',controller.deleteuser);
   
 module.exports = router;
